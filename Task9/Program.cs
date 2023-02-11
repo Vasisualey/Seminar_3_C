d68211abc9ded891f5e6606ccd773fa793c68228
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-11 14:47:22 +0300

    решил 9ю

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в два де пространстве
+
+Console.Clear();
+Console.Write("Введите Координату X1: ");
+int X1 = int.Parse(Console.ReadLine()!)!;
+Console.Write("Введите Координату X2: ");
+int X2 = int.Parse(Console.ReadLine()!)!;
+Console.Write("Введите Координату Y1: ");
+int Y1 = int.Parse(Console.ReadLine()!)!;
+Console.Write("Введите Координату Y1: ");
+int Y2 = int.Parse(Console.ReadLine()!)!;
+
+double result = Math.Sqrt(Math.Pow(X2-X1,2) + Math.Pow(Y2-Y1,2));
+Console.WriteLine("Расстояние между двумя точками равно " + result);
