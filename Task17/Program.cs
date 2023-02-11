728707a7696299cf6b30c301304a66f9aaa60eaf
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-11 12:51:04 +0300

    Добавил задачу 17

---
+++ Task17/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 17. Напишите программу, которая принимает на вход координаты точки
+// х и у, причем Х не равно 0 и не равно 0 и выдает номер четверти плоскости,
+// в которой находится эта точка.
+  
+Console.Clear();
+Console.Write("Введите Х: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y >0)
+{
+    Console.WriteLine("I");
+}
+if(X < 0 && Y >0)
+{
+    Console.WriteLine("II");
+}
+if(X < 0 && Y < 0)
+{
+    Console.WriteLine("III");
+}
+if(X > 0 && Y < 0)
+{
+    Console.WriteLine("IV");
+}
