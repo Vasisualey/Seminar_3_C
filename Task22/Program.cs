81dc44d59232b45b4b14eac20715d674a3935e2d
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-11 16:43:07 +0300

    решил 22ю

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу которая принимает на вход  число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int X = int.Parse(Console.ReadLine()!)!;
+int Counter = 1;
+if(X>0)
+{
+while(Counter <= X)
+{
+    Console.Write(Math.Pow(Counter,2)+" ");
+    Counter = Counter + 1;
+}
+}
+else
+{
+    while(Counter >= X)
+{
+    Console.Write(Math.Pow(Counter,2)+" ");
+    Counter = Counter - 1;
+}
+}
